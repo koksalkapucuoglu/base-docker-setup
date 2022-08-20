86b068279b82286469340910468bacfaf62ead24
koksal.kapucuoglu <koksalkapucuoglu@hotmail.com>
2022-08-20 17:02:14 +0300

    move mysql changes to mysql-docker branch

--- core/settings.py
+++ core/settings.py
@@ -72,18 +72,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-app-db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'db',
-        'PORT': 3306,
-    }
-}
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
